b9c1e07ecda3abf876369345ad69b0f868a3095f
Micha Davis <m.leigh.davis@gmail.com>
2022-04-03 23:40:55 -0700

    remove unused model and methods

--- src/routes/v2.js
+++ src/routes/v2.js
@@ -63,40 +63,4 @@ router.delete('/delete/:limiter', bearerAuth, authorize('delete'), async (reques
   }
 });
 
-// router.get('/:model', bearerAuth, authorize('read'), handleGetAll);
-// router.get('/:model/:id', bearerAuth, authorize('read'), handleGetOne);
-// router.post('/:model', bearerAuth, authorize('create'), handleCreate);
-// router.put('/:model/:id', bearerAuth, authorize('update'), handleUpdate);
-// router.delete('/:model/:id', bearerAuth, authorize('delete'), handleDelete);
-
-// async function handleGetAll(req, res) {
-//   let allRecords = await req.model.get();
-//   res.status(200).json(allRecords);
-// }
-
-// async function handleGetOne(req, res) {
-//   const id = req.params.id;
-//   let theRecord = await req.model.get(id);
-//   res.status(200).json(theRecord);
-// }
-
-// async function handleCreate(req, res) {
-//   let obj = req.body;
-//   let newRecord = await req.model.create(obj);
-//   res.status(201).json(newRecord);
-// }
-
-// async function handleUpdate(req, res) {
-//   const id = req.params.id;
-//   const obj = req.body;
-//   let updatedRecord = await req.model.update(id, obj);
-//   res.status(200).json(updatedRecord);
-// }
-
-// async function handleDelete(req, res) {
-//   let id = req.params.id;
-//   let deletedRecord = await req.model.delete(id);
-//   res.status(200).json(deletedRecord);
-// }
-
 module.exports = router;

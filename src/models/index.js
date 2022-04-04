aec1060251a0ce82695c33c36d823f40ffa6b8ef
Micha Davis <m.leigh.davis@gmail.com>
2022-04-03 23:34:18 -0700

    renamed model

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const queryModel = require('./queries/model.js');
+const limiterModel = require('./limiter/model.js');
 const Collection = require('./data-collection.js');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory:';
 
 const sequelize = new Sequelize(DATABASE_URL);
-const query = queryModel(sequelize, DataTypes);
+const limiter = limiterModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
-  query: new Collection(query),
+  limiter: new Collection(limiter),
 };
 
 // {
